ff84cc5397e09d1f3977d1d695301e21228bfa3b
Roger Camargo <huogerac@gmail.com>
2011-10-31 23:09:11 -0200

    step 05: new method - htmlstatement

--- code01-movie/src/main/java/br/com/bc/movie/Customer.java
+++ code01-movie/src/main/java/br/com/bc/movie/Customer.java
@@ -1,6 +1,7 @@
 package br.com.bc.movie;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
 public class Customer {
@@ -37,6 +38,23 @@ public class Customer {
 
 		return result;
 	}
+	
+	public String htmlStatement() {
+
+		String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+		for (Rental rental : rentals) {
+						// show figures for each rental
+			result += rental.getMovie().getTitle() + ": "
+					+ String.valueOf(rental.getCharge()) + "<BR>\n";
+		}
+		// add footer lines
+		result += "<P>You owe <EM>" + String.valueOf(getTotalCharge())
+				+ "</EM><P>\n";
+		result += "On this rental you earned <EM>"
+				+ String.valueOf(getTotalFrequentRenterPoints())
+				+ "</EM> frequent renter points<P>";
+		return result;
+	}	
 
 	private double getTotalCharge() {
 		double result = 0;
--- code01-movie/src/test/java/br/com/bc/movie/MovieTest.java
+++ code01-movie/src/test/java/br/com/bc/movie/MovieTest.java
@@ -32,5 +32,32 @@ public class MovieTest {
 				     "You earned 4 frequent renter points", statement);
 
 	}
+	
+	@Test
+	public void statementHtmlTest() {
+
+		Movie marvel1 = new Movie("Iron Man", Movie.REGULAR);
+		Movie marvel2 = new Movie("X-Men Origins: Wolverine", Movie.REGULAR);
+		Movie marvel3 = new Movie("Iron Man 2", Movie.NEW_RELEASE);
+
+		Rental rental1 = new Rental(marvel1, 4);
+		Rental rental2 = new Rental(marvel2, 4);
+		Rental rental3 = new Rental(marvel3, 4);
+
+		Customer customer1 = new Customer("Roger");
+		customer1.addRental(rental1);
+		customer1.addRental(rental2);
+		customer1.addRental(rental3);
+
+		String statement = customer1.htmlStatement();
+		
+		assertEquals("<H1>Rentals for <EM>Roger</EM></H1><P>\n" +
+		             "Iron Man: 5.0<BR>\n" +
+				     "X-Men Origins: Wolverine: 5.0<BR>\n" + 
+		             "Iron Man 2: 12.0<BR>\n" + 
+				     "<P>You owe <EM>22.0</EM><P>\n" +
+				     "On this rental you earned <EM>4</EM> frequent renter points<P>", statement);
+
+	}	
 
 }
