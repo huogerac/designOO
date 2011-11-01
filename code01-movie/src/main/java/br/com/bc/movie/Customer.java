942a6e4d1354e613618f9a2dce13a005d3e5cfe6
Roger Camargo <huogerac@gmail.com>
2011-11-01 13:07:51 -0200

    step 06: conditional logic

--- code01-movie/src/main/java/br/com/bc/movie/Customer.java
+++ code01-movie/src/main/java/br/com/bc/movie/Customer.java
@@ -1,7 +1,6 @@
 package br.com.bc.movie;
 
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 
 public class Customer {
---
+++ code01-movie/src/main/java/br/com/bc/movie/Price.java
@@ -0,0 +1,52 @@
+package br.com.bc.movie;
+
+public abstract class Price {
+	abstract int getPriceCode();
+
+	abstract double getCharge(int daysRented);
+
+	public int getFrequentRenterPoints(int daysRented) {
+		return 1;
+	}
+	
+}
+
+class ChildrensPrice extends Price {
+	int getPriceCode() {
+		return Movie.CHILDRENS;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 1.5;
+		if (daysRented > 3)
+			result += (daysRented - 3) * 1.5;
+		return result;
+	}
+}
+
+class NewReleasePrice extends Price {
+	int getPriceCode() {
+		return Movie.NEW_RELEASE;
+	}
+
+	public double getCharge(int daysRented) {
+		return daysRented * 3;
+	}
+	public int getFrequentRenterPoints(int daysRented) {
+		return (daysRented > 1) ? 2: 1;
+	}
+}
+
+class RegularPrice extends Price {
+	int getPriceCode() {
+		return Movie.REGULAR;
+	}
+
+	public double getCharge(int daysRented) {
+		double result = 2;
+		if (daysRented > 2)
+			result += (daysRented - 2) * 1.5;
+		return result;
+	}
+
+}
