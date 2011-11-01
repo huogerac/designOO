7efbaa8ccb7a8ab6ae990c832ae5e1ed2851ec6a
Roger Camargo <huogerac@gmail.com>
2011-10-31 22:58:30 -0200

    step 03: extrat getFrequentRenterPoints to Rental

--- code01-movie/src/main/java/br/com/bc/movie/Customer.java
+++ code01-movie/src/main/java/br/com/bc/movie/Customer.java
@@ -29,13 +29,7 @@ public class Customer {
 		for (Rental rental : rentals) {
 			double thisAmount = rental.getCharge();
 			
-			// add frequent renter points
-			frequentRenterPoints++;
-			
-			// add bonus for a two day new release rental
-			if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& rental.getDaysRented() > 1)
-				frequentRenterPoints++;
+			frequentRenterPoints += rental.getFrequentRenterPoints();
 			
 			// show figures for this rental
 			result += "\t" + rental.getMovie().getTitle() + "\t"
